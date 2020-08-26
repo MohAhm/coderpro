8ed0b2af49240ff7ad75d7f5b380ba9505ec28ce
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-08-26 14:06:32 +0200

    Permutations

---
+++ permutations.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    # Time: O(n!)
+    # Space: O(n!)
+
+    def _permuteHelper(self, nums, start=0):
+        if start == len(nums) - 1:
+            return [nums[:]]
+
+        result = []
+
+        for i in range(start, len(nums)):
+            nums[start], nums[i] = nums[i], nums[start]
+            result += self._permuteHelper(nums, start + 1)
+            nums[start], nums[i] = nums[i], nums[start]
+
+        return result
+
+    def permute(self, nums):
+        return self._permuteHelper(nums)
+
+    def permute2(self, nums, values=[]):
+        if not nums:
+            return [values]
+
+        result = []
+        for i in range(len(nums)):
+            result += self.permute2(nums[:i] + nums[i+1:], values + [nums[i]])
+
+        return result
+
+
+print(Solution().permute([1, 2, 3]))
+print(Solution().permute2([1, 2, 3]))
